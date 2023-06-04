4d3a084a0a61a55b39e2ab51981f09ae08bfe04e
AndreaFerrini <andrea.ferrini90@gmail.com>
2023-06-04 13:30:21 +0200

    creazione struttura + header

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+include_once __DIR__ . "/partials/header.php";
+include_once __DIR__ . "/partials/main.php";
+include_once __DIR__ . "/partials/footer.php";
+
+?>
---
+++ partials/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tuscany Pet Market</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href=".\assets\style.css">
+</head>
+<body>
+    <!-- Header Menu of the Page -->
+    <header>
+        <div class="container d-flex justify-content-around">
+            <!-- Top header menu containing
+                logo and Navigation bar -->
+            <div id="top-header"class="d-flex justify-content-around align-items-center">
+                <!-- Logo -->
+                <div id="logo">
+                    <img src="./assets/logo pet store.png" />
+                </div>   
+                            
+                <!-- Navigation Menu -->
+                <nav>
+                    <ul class="d-flex flex-column pe-5">
+                        <button type="button" class="btn btn-warning">Home</button>
+                        <button type="button" class="btn btn-warning">Our Products</button>
+                        <button type="button" class="btn btn-warning">Contact Us</button>
+                    </ul>
+                </nav>
+            </div>   
+        </div>
+    </header>
